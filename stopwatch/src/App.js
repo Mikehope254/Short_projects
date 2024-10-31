0abc93159ef8a1c0442824836a484d0db5323a85
Mikehope254 <mikehope1738@gmail.com>
2024-10-31 20:32:21 +0300

    Stopwatch complete

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,12 +1,12 @@
-import { useEffect, useState, usestate } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [time, setTime] = usestate(0);
+  const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
 
   useEffect(() => {
-    let interval;
+    let interval = null;
     if (running) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime + 10);
@@ -17,38 +17,42 @@ function App() {
     return () => clearInterval(interval);
   }, [running]);
   return (
-    <div>
-      <h1>Stopwatch</h1>
-      <div>
-        <span>{"0" + Math.floor((time / 60000) % 60)}</span>
-        {/*The time is calculated by dividing the time by the number of miliseconds for each minute(1 minute is 60000ms) with the remainder operator %60 restricting it  between 0-59*/}
-        <span>{"0" + Math.floor((time / 1000) % 60)}</span>
-        {/*This calculates the seconds 1s = 1000ms*/}
+    <div className="main-container">
+      <h1 className="stopwatch-title">Stopwatch</h1>
+      <div className="timer-display">
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
         <span>{"0" + ((time / 10) % 100)}</span>
-        {/*time/10 gives centiseconds making the stop watch give 2-digit precion for ms 1cs=10ms */}
       </div>
-      <div>
-        <buttons
-          onClick={() => {
-            setRunning(true);
-          }}
-        >
-          Start
-        </buttons>
-        <buttons
-          onClick={() => {
-            setRunning(false);
-          }}
-        >
-          Stop
-        </buttons>
-        <buttons
+      <div className="button-container">
+        {running ? (
+          <button
+            className="button button-stop"
+            onClick={() => {
+              setRunning(false);
+            }}
+          >
+            Stop
+          </button>
+        ) : (
+          <button
+            className="button button-start"
+            onClick={() => {
+              setRunning(true);
+            }}
+          >
+            Start
+          </button>
+        )}
+
+        <button
+          className="button button-reset"
           onClick={() => {
             setTime(0);
           }}
         >
           Reset
-        </buttons>
+        </button>
       </div>
     </div>
   );
